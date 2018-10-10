3ce399fef99949e3a364a7ee02339ccbe1d11bf4
Wun Chiou <wun@utdallas.edu>
2018-10-10 16:19:54 -0500

    Gulp: removes unused gulp-concat dependency

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
 
 var paths = {
   public: {
